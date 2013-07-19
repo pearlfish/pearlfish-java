3884142d5db9c86f8a97682e33045c41fc4a965d
npryce <github@natpryce.com>
2013-07-19 17:29:10 +0100

    Example actually tests something!

--- java/test/com/natpryce/pearlfish/example/BasicArithmeticExample.java
+++ java/test/com/natpryce/pearlfish/example/BasicArithmeticExample.java
@@ -1,12 +1,14 @@
 package com.natpryce.pearlfish.example;
 
 
+import com.natpryce.pearlfish.Scenario;
 import com.natpryce.pearlfish.junit.Pearlfish;
 import org.junit.Rule;
 import org.junit.Test;
 import org.rococoa.okeydoke.junit.ApprovalsRule;
 
 import java.io.IOException;
+import java.util.List;
 
 import static com.natpryce.pearlfish.Results.results;
 import static com.natpryce.pearlfish.Results.scenario;
@@ -18,9 +20,13 @@ public class BasicArithmeticExample {
     @Test
     public void addition() throws IOException {
         approvals.assertApproved(results(
-                scenario("simple add", asList(1, 2), 3),
-                scenario("zero left",  asList(0, 2), 2),
-                scenario("zero right", asList(1, 0), 1),
-                scenario("zero both",  asList(0, 0), 0)));
+                addition("simple add", 1, 2),
+                addition("zero left", 0, 2),
+                addition("zero right", 1, 0),
+                addition("zero both", 0, 0)));
+    }
+
+    private Scenario<List<Integer>, Integer> addition(String what, int x, int y) {
+        return scenario(what, asList(x, y), x + y);
     }
 }
