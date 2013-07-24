720b21b3f658538a062d90ecd45a14132b8df019
Nat Pryce <sw@natpryce.com>
2013-07-24 22:45:29 +0100

    Refactoring API call to create an approver without specifying the file format -- uses plain text

--- src/com/natpryce/pearlfish/junit/Pearlfish.java
+++ src/com/natpryce/pearlfish/junit/Pearlfish.java
@@ -2,49 +2,15 @@ package com.natpryce.pearlfish.junit;
 
 import com.natpryce.pearlfish.formats.Format;
 import com.natpryce.pearlfish.formats.Formats;
-import com.natpryce.pearlfish.formats.MissingTemplateException;
-import org.rococoa.okeydoke.Approver;
-import org.rococoa.okeydoke.ApproverFactory;
-import org.rococoa.okeydoke.Formatter;
-import org.rococoa.okeydoke.Reporters;
+import com.natpryce.pearlfish.internal.PearlfishApproverFactory;
 import org.rococoa.okeydoke.junit.ApprovalsRule;
-import org.rococoa.okeydoke.sources.FileSystemSourceOfApproval;
-
-import java.io.File;
 
 public class Pearlfish implements Formats {
-    public static ApprovalsRule approvalRule(final String srcRoot, final Format format) {
-        return new ApprovalsRule(new PearlfishApproverFactory(srcRoot, format));
+    public static ApprovalsRule approvalRule(final String srcRoot) {
+        return new ApprovalsRule(new PearlfishApproverFactory(srcRoot, PLAIN_TEXT));
     }
 
-    private static class PearlfishApproverFactory implements ApproverFactory {
-        private final String srcRoot;
-        private final Format preferredFormat;
-
-        public PearlfishApproverFactory(String srcRoot, Format preferredFormat) {
-            this.srcRoot = srcRoot;
-            this.preferredFormat = preferredFormat;
-        }
-
-        public Approver create(String testName, Class<?> testClass) {
-            Formatter<Object,String> formatter;
-            String fileExtension;
-            try {
-                formatter = preferredFormat.formatterFor(testName, testClass);
-                fileExtension = preferredFormat.fileExtension();
-            }
-            catch (MissingTemplateException e) {
-                formatter = YAML.formatterFor(testName, testClass);
-                fileExtension = YAML.fileExtension();
-            }
-
-            return new Approver(
-                    testName,
-                    new FileSystemSourceOfApproval(
-                            new File(srcRoot), testClass.getPackage()).
-                            withTypeExtension(fileExtension),
-                    formatter,
-                    Reporters.reporter());
-        }
+    public static ApprovalsRule approvalRule(final String srcRoot, final Format format) {
+        return new ApprovalsRule(new PearlfishApproverFactory(srcRoot, format));
     }
 }
