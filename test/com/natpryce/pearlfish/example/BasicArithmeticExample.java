30863c2e9107b735cdde84eed9d4604e6ccbabcb
Nat Pryce <sw@natpryce.com>
2013-07-31 09:33:46 +0100

    More realistic example

--- test/com/natpryce/pearlfish/example/BasicArithmeticExample.java
+++ test/com/natpryce/pearlfish/example/BasicArithmeticExample.java
@@ -1,6 +1,7 @@
 package com.natpryce.pearlfish.example;
 
 
+import com.natpryce.pearlfish.Scenario;
 import com.natpryce.pearlfish.junit.Pearlfish;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,26 +13,63 @@ import java.math.BigInteger;
 import static com.natpryce.pearlfish.Results.*;
 import static com.natpryce.pearlfish.formats.Formats.MARKDOWN;
 
+@SuppressWarnings("unchecked")
 public class BasicArithmeticExample {
     public @Rule ApprovalsRule approvals = Pearlfish.approvalRule("test", MARKDOWN);
 
+    /** This is the code that we are testing
+     */
+    Calculator calculator = new Calculator();
+
     @Test
-    @SuppressWarnings("unchecked")
     public void addition() throws IOException {
         approvals.assertApproved(results(
                 section("basic",
-                        scenario("simple add", new Operands(1, 2), BigInteger.valueOf(3)),
-                        scenario("zero left", new Operands(0, 2), BigInteger.valueOf(2)),
-                        scenario("zero right", new Operands(1, 0), BigInteger.valueOf(1)),
-                        scenario("zero both", new Operands(0, 0), BigInteger.valueOf(0))),
+                        addition("simple add", 1, 2),
+                        addition("zero left", 0, 2),
+                        addition("zero right", 1, 0),
+                        addition("zero both", 0, 0)),
                 section("negative",
-                        scenario("negative left", new Operands(-4, 2), BigInteger.valueOf(-2)),
-                        scenario("negative right", new Operands(5, -4), BigInteger.valueOf(1)),
-                        scenario("negative left and zero", new Operands(-1, 0), BigInteger.valueOf(-1)),
-                        scenario("zero and negative right", new Operands(0, -6), BigInteger.valueOf(-6)),
-                        scenario("both negative", new Operands(-4, -9), BigInteger.valueOf(-13))),
+                        addition("negative left", -4, 2),
+                        addition("negative right", 5, -4),
+                        addition("negative left and zero", -1, 0),
+                        addition("zero and negative right", 0, -6),
+                        addition("both negative", -4, -9)),
                 section("large",
-                        scenario("large addition", new Operands(Integer.MAX_VALUE, Integer.MAX_VALUE), BigInteger.valueOf(Integer.MAX_VALUE).add(BigInteger.valueOf(Integer.MAX_VALUE))),
-                        scenario("large negative numbers", new Operands(Integer.MIN_VALUE, Integer.MIN_VALUE), BigInteger.valueOf(Integer.MIN_VALUE).add(BigInteger.valueOf(Integer.MIN_VALUE))))));
+                        addition("large addition", Integer.MAX_VALUE, Integer.MAX_VALUE),
+                        addition("large negative numbers", Integer.MIN_VALUE, Integer.MIN_VALUE))));
+    }
+
+    @Test
+    public void multiplication() throws IOException {
+        approvals.assertApproved(results(
+                section("basic",
+                        multiplication("simple multiplication", 2, 5),
+                        multiplication("zero left", 0, 2),
+                        multiplication("zero right", 1, 0),
+                        multiplication("zero both", 0, 0)),
+                section("negative",
+                        multiplication("negative left", -4, 2),
+                        multiplication("negative right", 5, -4),
+                        multiplication("negative left and zero", -1, 0),
+                        multiplication("zero and negative right", 0, -6),
+                        multiplication("both negative", -4, -9)),
+                section("large",
+                        multiplication("large addition", Integer.MAX_VALUE, 2),
+                        multiplication("large negative numbers", Integer.MIN_VALUE, 2))));
+    }
+
+    private Scenario<Operands, BigInteger> addition(final String description, int x, int y) {
+        calculator.push(x);
+        calculator.push(y);
+        calculator.add();
+        return scenario(description, new Operands(x, y), calculator.pop());
+    }
+
+    private Scenario<Operands, BigInteger> multiplication(final String description, int x, int y) {
+        calculator.push(x);
+        calculator.push(y);
+        calculator.mul();
+        return scenario(description, new Operands(x, y), calculator.pop());
     }
 }
---
+++ test/com/natpryce/pearlfish/example/Calculator.java
@@ -0,0 +1,32 @@
+package com.natpryce.pearlfish.example;
+
+import java.math.BigInteger;
+import java.util.Stack;
+
+public class Calculator {
+    private final Stack<BigInteger> operands = new Stack<BigInteger>();
+
+    public void push(BigInteger operand) {
+        operands.push(operand);
+    }
+
+    public void push(long operand) {
+        operands.push(BigInteger.valueOf(operand));
+    }
+
+    public BigInteger top() {
+        return operands.peek();
+    }
+
+    public BigInteger pop() {
+        return operands.pop();
+    }
+
+    public void add() {
+        push(pop().add(pop()));
+    }
+
+    public void mul() {
+        push(pop().multiply(pop()));
+    }
+}
