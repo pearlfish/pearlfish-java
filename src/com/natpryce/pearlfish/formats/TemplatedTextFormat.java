65e14fa5a77336014e354b3429af5b8f78eb8a83
Nat Pryce <sw@natpryce.com>
2013-09-02 08:51:20 +0100

    Remove unused methods

--- src/com/natpryce/pearlfish/formats/TemplatedTextFormat.java
+++ src/com/natpryce/pearlfish/formats/TemplatedTextFormat.java
@@ -1,17 +1,12 @@
 package com.natpryce.pearlfish.formats;
 
-import com.google.common.io.Resources;
 import com.natpryce.pearlfish.Format;
 import com.natpryce.pearlfish.FormatType;
-import com.samskivert.mustache.Escaping;
-import com.samskivert.mustache.Mustache;
 import com.samskivert.mustache.Template;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 public class TemplatedTextFormat implements Format<Object> {
     private final Template template;
@@ -24,17 +19,6 @@ public class TemplatedTextFormat implements Format<Object> {
         this.postTemplateFilter = postTemplateFilter;
         this.fileExtension = fileExtension;
         this.fileType = fileType;
-    }
-
-    public static TemplatedTextFormat fromResource(Class<?> relativeToClass, String baseName, Charset charset, Escaping valueEscaping, TextFilter postTemplateFilter, String fileExtension, final FormatType fileType) {
-        final String resourceName = relativeToClass.getSimpleName() + "." + baseName + fileExtension + ".template";
-
-        return new TemplatedTextFormat(
-                loadTemplate(relativeToClass, resourceName, charset, valueEscaping),
-                postTemplateFilter,
-                fileExtension,
-                fileType);
-
     }
 
     @Override
@@ -52,20 +36,5 @@ public class TemplatedTextFormat implements Format<Object> {
         OutputStreamWriter writer = new OutputStreamWriter(output);
         writer.write(postTemplateFilter.filter(template.execute(value)));
         writer.flush();
-    }
-
-    private static Template loadTemplate(Class<?> testClass, String templateName, Charset charset, Escaping valueEscaping) {
-        try {
-            URL resource = testClass.getResource(templateName);
-            if (resource == null) {
-                throw new MissingTemplateException(templateName);
-            }
-
-            return Mustache.compiler()
-                    .escaping(valueEscaping)
-                    .compile(Resources.toString(resource, charset));
-        } catch (IOException e) {
-            throw new IllegalArgumentException("invalid template " + templateName, e);
-        }
     }
 }
